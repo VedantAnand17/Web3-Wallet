eb8986325acd65f0be78ee70e28f0182cf7a1336
Ayush Deb <ayushdeb848@gmail.com>
2024-10-02 22:19:25 +0530

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,17 +10,21 @@ const Navbar = () => {
         <div className="flex items-center space-x-3">
           <Link to="/" className="flex items-center space-x-3">
             <Globe className="w-8 h-8 text-white" />
-            <h1 className="text-3xl font-extrabold tracking-tight">
-              Web3-Wallet
-            </h1>
+            <h1 className="text-2xl sm:text-3xl font-bold tracking-tight">Web3-Wallet</h1>
           </Link>
         </div>
-        
-        {/* DAppBrowser Call-to-Action Button */}
-        <div className="hidden md:flex">
+
+        {/* Home and DAppBrowser Buttons */}
+        <div className="flex space-x-2">
+          <Link
+            to="/"
+            className="bg-indigo-500 hover:bg-gray-600 text-white py-1 px-2 rounded-md shadow-md transition duration-300 ease-in-out text-xs sm:text-sm md:text-base"
+          >
+            Home
+          </Link>
           <Link
             to="/dapp-browser"
-            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out"
+            className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 rounded-md shadow-md transition duration-300 ease-in-out text-xs sm:text-sm md:text-base"
           >
             DAppBrowser
           </Link>
